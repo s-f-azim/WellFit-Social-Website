9a561977fc3482d31508ba9853467bd2dae46842
ahmed-alahmadi <a-e-a-1999@hotmail.com>
2021-02-20 01:23:07 +0000

    Add security meassures for the API
    XSS and santanizing the data been added and also additionally security
    headers

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,27 +6,41 @@ import cookieParser from "cookie-parser";
 import ErrorResponse from "./utils/errorResponse.js";
 import userRoutes from "./routes/user.js";
 import passport from "../config/passport-setup.js";
-//connect to the database
+import santanize from "express-mongo-sanitize";
+import helmet from "helmet";
+import xss from "xss-clean";
+// connect to the database
 connectDb();
 
-//create the app and setup
+// create the app and setup
 const app = express();
 app.use(express.json());
 
 // cookie parser
 app.use(cookieParser());
-//passport setup
+
+// santanize data
+app.use(santanize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+
+app.use(xss());
+
+// passport setup
 app.use(passport.initialize());
 
-//cors
+// cors
 if (process.env.NODE_ENV === "DEVELOPMENT") {
   app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
 
-//routes
+// routes
 app.use("/api/users", userRoutes);
 
-//404 if the route doesn't match
+// 404 if the route doesn't match
 app.all("*", (_, res) => {
   throw new ErrorResponse("Resource not found on this server", 404);
 });
