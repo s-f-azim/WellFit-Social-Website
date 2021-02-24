3c70b0b49fbd3e5e6d657fc05e087c91a737589a
Alexandre Chouman <alexandre.chouman@outlook.com>
2021-02-24 17:45:08 +0100

    add tiny visual fixes and start new tab

--- frontend/components/ProfileBar.jsx
+++ frontend/components/ProfileBar.jsx
@@ -25,35 +25,37 @@ const ProfileBar = ({ profileOpen, setProfileOpen }) => {
   };
   return (
     <div className={`profile-bar ${profileOpen ? "active" : ""}`}>
-      <CloseOutlined onClick={() => setProfileOpen(false)} />
-      <Avatar
-        className="profile-pic"
-        size={{ xs: 59, sm: 68, md: 75, lg: 85, xl: 95, xxl: 130 }}
-        icon={<AntDesignOutlined />}
-      />
-      <h1>{user.name}</h1>
-      <div className="item">
-        <HistoryOutlined />
-        <h1>Purchase history</h1>
-      </div>
-      <div className="item">
-        <EditOutlined />
-        <h1 onClick={() => setProfileOpen(false)}>
-          <Link href="/editProfile" >Edit profile</Link>
-        </h1>
-      </div>
-      <div className="item">
-        <HistoryOutlined />
-        <h1>Change goals</h1>
-      </div>
-      <div className="item">
-        <SettingOutlined />
-        <h1>Settings</h1>
-      </div>
-      <div onClick={logout} className="item logout">
-        <LogoutOutlined />
-        <h1>Logout</h1>
-      </div>
+    <CloseOutlined onClick={() => setProfileOpen(false)} />
+    <Avatar
+    className="profile-pic"
+    size={{ xs: 59, sm: 68, md: 75, lg: 85, xl: 95, xxl: 130 }}
+    icon={<AntDesignOutlined />}
+    />
+    <h1>{user.name}</h1>
+    <div className="item">
+    <HistoryOutlined />
+    <h1>Purchase history</h1>
+    </div>
+    <div className="item">
+    <EditOutlined />
+    <h1>
+    <div onClick={() => setProfileOpen(false)} >
+    <Link href="/editProfile" >Edit profile</Link>
+    </div>
+    </h1>
+    </div>
+    <div className="item">
+    <HistoryOutlined />
+    <h1>Change goals</h1>
+    </div>
+    <div className="item">
+    <SettingOutlined />
+    <h1>Settings</h1>
+    </div>
+    <div onClick={logout} className="item logout">
+    <LogoutOutlined />
+    <h1>Logout</h1>
+    </div>
     </div>
   );
 };
--- frontend/pages/editProfile.jsx
+++ frontend/pages/editProfile.jsx
@@ -72,140 +72,143 @@ const editProfilePage = () => {
 
   return (
     <Row type="flex" justify="center" align="middle">
-      <Card>
-        <Tabs defaultActiveKey="1">
-          <TabPane tab="Add Info" key="1">
-            <Form
-              {...formItemLayout}
-              form={form}
-              name="Update my info"
-              onFinish={onFinish}
-              scrollToFirstError
-            >
-              <Space direction="vertical" size="middle">
-                {hasError && (
-                  <Alert
-                    type="error"
-                    message="something went wrong, please try again"
-                    banner
-                  />
-                )}
-                <h1>Modify profile information</h1>
-                <Alert
-                  message="This helps professionals know about you right away"
-                  type="info"
-                  showIcon
-                />
-                <Form.Item name="gender" label="Gender">
-                  <Select placeholder="Select your gender">
-                    <Option value="Male">Male</Option>
-                    <Option value="Female">Female</Option>
-                    <Option value="Non-Binary"> Non-Binary</Option>
-                    <Option value="Prefer not to say">Prefer not to say</Option>
-                  </Select>
-                </Form.Item>
+    <Card>
+    <Tabs defaultActiveKey="1">
+    <TabPane tab="Basic Info" key="1">
+    <Form
+    {...formItemLayout}
+    form={form}
+    name="Update my info"
+    onFinish={onFinish}
+    scrollToFirstError
+    >
+    <Space direction="vertical" size="middle">
+    {hasError && (
+      <Alert
+      type="error"
+      message="something went wrong, please try again"
+      banner
+      />
+    )}
+    <h1>Modify profile information</h1>
+    <Alert
+    message="This helps professionals know about you right away"
+    type="info"
+    showIcon
+    />
+    <Form.Item name="gender" label="Gender">
+    <Select placeholder="Select your gender">
+    <Option value="Male">Male</Option>
+    <Option value="Female">Female</Option>
+    <Option value="Non-Binary"> Non-Binary</Option>
+    <Option value="Prefer not to say">Prefer not to say</Option>
+    </Select>
+    </Form.Item>
 
-                <Form.Item name="location" label="Location">
-                  <Select placeholder="Select your location">
-                    <Option value="Europe">Europe</Option>
-                    <Option value="Asia">Asia</Option>
-                    <Option value="North America">North America</Option>
-                    <Option value="South America">South America</Option>
-                    <Option value="Australia">Australia</Option>
-                    <Option value="Africa">Africa</Option>
-                  </Select>
-                </Form.Item>
+    <Form.Item name="location" label="Location">
+    <Select placeholder="Select your location">
+    <Option value="Europe">Europe</Option>
+    <Option value="Asia">Asia</Option>
+    <Option value="North America">North America</Option>
+    <Option value="South America">South America</Option>
+    <Option value="Australia">Australia</Option>
+    <Option value="Africa">Africa</Option>
+    </Select>
+    </Form.Item>
 
-                <Form.Item
-                  name="birthday"
-                  label="Birthday"
-                  rules={[
-                    {
-                      type: "object",
-                      message: "Please select your birthday",
-                    },
-                  ]}
-                >
-                  <DatePicker disabledDate={(d) => !d || d.isAfter(date)} />
-                </Form.Item>
+    <Form.Item
+    name="birthday"
+    label="Birthday"
+    rules={[
+      {
+        type: "object",
+        message: "Please select your birthday",
+      },
+    ]}
+    >
+    <DatePicker disabledDate={(d) => !d || d.isAfter(date)} />
+    </Form.Item>
 
-                <Form.Item name="nickname" label="Nickname">
-                  <Input />
-                </Form.Item>
+    <Form.Item name="nickname" label="Nickname">
+    <Input />
+    </Form.Item>
 
-                <Form.Item name="bio" label="Bio">
-                  <Input.TextArea />
-                </Form.Item>
+    <Form.Item name="bio" label="Bio">
+    <Input.TextArea />
+    </Form.Item>
 
-                <Form.Item {...tailFormItemLayout}>
-                  <Button type="primary" htmlType="submit">
-                    Update my info
-                  </Button>
-                </Form.Item>
-              </Space>
-            </Form>
-          </TabPane>
+    <Form.Item {...tailFormItemLayout}>
+    <Button type="primary" htmlType="submit">
+    Update my info
+    </Button>
+    </Form.Item>
+    </Space>
+    </Form>
+    </TabPane>
 
-          <TabPane tab="Edit Account" key="2">
-            <Form
-              {...formItemLayout}
-              form={form}
-              name="Edit my info"
-              onFinish={onFinish}
-              scrollToFirstError
-            >
-              <Space direction="vertical" size="middle">
-                {hasError && (
-                  <Alert
-                    type="error"
-                    message="Make sure both passwords match and are over 8 characters"
-                    banner
-                  />
-                )}
-                <h1>Change your email or password</h1>
-              </Space>
-              <Form.Item
-                name="email"
-                label="New email"
-                rules={[
-                  {
-                    type: "email",
-                    message: "Invalid Email",
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
-              <Form.Item name="password" label="New password" hasFeedback>
-                <Input.Password />
-              </Form.Item>
-              <Form.Item
-                name="confirm"
-                label="Confirm Password"
-                dependencies={["password"]}
-                hasFeedback
-                rules={[
-                  ({ getFieldValue }) => ({
-                    validator(_, value) {
-                      if (!value || getFieldValue("password") === value) {
-                        return Promise.resolve();
-                      }
-                      return Promise.reject("Sorry the passwords do not match");
-                    },
-                  }),
-                ]}
-              >
-                <Input.Password />
-              </Form.Item>
-              <Form.Item className="submit" {...tailFormItemLayout}>
-                <Button type="primary" htmlType="submit">
-                  Confirm
-                </Button>
-              </Form.Item>
-            </Form>
-          </TabPane>
-        </Tabs>
-      </Card>
+    <TabPane tab="Detailed info" key="2">
+    </TabPane>
+
+    <TabPane tab="Edit Credentials" key="3">
+    <Form
+    {...formItemLayout}
+    form={form}
+    name="Edit my info"
+    onFinish={onFinish}
+    scrollToFirstError
+    >
+    <Space direction="vertical" size="middle">
+    {hasError && (
+      <Alert
+      type="error"
+      message="Make sure both passwords match and are over 8 characters"
+      banner
+      />
+    )}
+    <h1>Change your email or password</h1>
+    </Space>
+    <Form.Item
+    name="email"
+    label="New email"
+    rules={[
+      {
+        type: "email",
+        message: "Invalid Email",
+      },
+    ]}
+    >
+    <Input />
+    </Form.Item>
+    <Form.Item name="password" label="New password" hasFeedback>
+    <Input.Password />
+    </Form.Item>
+    <Form.Item
+    name="confirm"
+    label="Confirm Password"
+    dependencies={["password"]}
+    hasFeedback
+    rules={[
+      ({ getFieldValue }) => ({
+        validator(_, value) {
+          if (!value || getFieldValue("password") === value) {
+            return Promise.resolve();
+          }
+          return Promise.reject("Sorry the passwords do not match");
+        },
+      }),
+    ]}
+    >
+    <Input.Password />
+    </Form.Item>
+    <Form.Item className="submit" {...tailFormItemLayout}>
+    <Button type="primary" htmlType="submit">
+    Confirm
+    </Button>
+    </Form.Item>
+    </Form>
+    </TabPane>
+    </Tabs>
+    </Card>
     </Row>
   );
 };
