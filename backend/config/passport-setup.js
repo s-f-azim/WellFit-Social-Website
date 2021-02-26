7c61c280ee889f3ff9918f23cc4a6749c9df8c75
ahmed-alahmadi <a-e-a-1999@hotmail.com>
2021-02-26 13:53:55 +0000

    Add test env

--- backend/config/passport-setup.js
+++ backend/config/passport-setup.js
@@ -45,76 +45,77 @@ passport.use(
     }
   )
 );
-// Google oauth strategy
-passport.use(
-  new googleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${process.env.SERVER_API_URL}/users/oauth/google/redirect`,
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      const currentUser = await User.findOne({ googleId: profile.id });
-      // check if the person has logged with google before
-      if (currentUser) {
-        done(null, currentUser);
-      } else {
-        await User.create({
-          googleId: profile.id,
-          email: profile.emails[0].value,
-          name: `${profile.name.givenName} ${profile.name.familyName}`,
-        });
+if (process.env.NODE_ENV !== "TEST") {
+  // Google oauth strategy
+  passport.use(
+    new googleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: `${process.env.SERVER_API_URL}/users/oauth/google/redirect`,
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        const currentUser = await User.findOne({ googleId: profile.id });
+        // check if the person has logged with google before
+        if (currentUser) {
+          done(null, currentUser);
+        } else {
+          await User.create({
+            googleId: profile.id,
+            email: profile.emails[0].value,
+            name: `${profile.name.givenName} ${profile.name.familyName}`,
+          });
+        }
       }
-    }
-  )
-);
-// Instagram oauth strategy
-passport.use(
-  new instagramStrategy(
-    {
-      clientID: process.env.INSTA_CLIENT_ID,
-      clientSecret: process.env.INSTA_CLIENT_SECRET,
-      callbackURL: `${process.env.SERVER_API_URL}/users/oauth/instagram/redirect`,
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      const currentUser = await User.findOne({ instaID: profile.id });
-      // check if the person has logged with google before
-      if (currentUser) {
-        done(null, currentUser);
-      } else {
-        await User.create({
-          instaId: profile.id,
-          email: profile.emails[0].value,
-          name: `${profile.name.givenName} ${profile.name.familyName}`,
-        });
+    )
+  );
+  // Instagram oauth strategy
+  passport.use(
+    new instagramStrategy(
+      {
+        clientID: process.env.INSTA_CLIENT_ID,
+        clientSecret: process.env.INSTA_CLIENT_SECRET,
+        callbackURL: `${process.env.SERVER_API_URL}/users/oauth/instagram/redirect`,
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        const currentUser = await User.findOne({ instaID: profile.id });
+        // check if the person has logged with google before
+        if (currentUser) {
+          done(null, currentUser);
+        } else {
+          await User.create({
+            instaId: profile.id,
+            email: profile.emails[0].value,
+            name: `${profile.name.givenName} ${profile.name.familyName}`,
+          });
+        }
       }
-    }
-  )
-);
-// Facebook oauth strategy
-passport.use(
-  new facebookStrategy(
-    {
-      clientID: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-      callbackURL: `${process.env.SERVER_API_URL}/users/oauth/facebook/redirect/`,
-      profileFields: ["email", "name"],
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      const currentUser = await User.findOne({ facebookId: profile.id });
-      // check if the person has logged with google before
-      if (currentUser) {
-        done(null, currentUser);
-      } else {
-        const { email, first_name, last_name } = profile._json;
-        await User.create({
-          facebookId: profile.id,
-          email: email,
-          name: `${first_name} ${last_name}`,
-        });
+    )
+  );
+  // Facebook oauth strategy
+  passport.use(
+    new facebookStrategy(
+      {
+        clientID: process.env.FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        callbackURL: `${process.env.SERVER_API_URL}/users/oauth/facebook/redirect/`,
+        profileFields: ["email", "name"],
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        const currentUser = await User.findOne({ facebookId: profile.id });
+        // check if the person has logged with google before
+        if (currentUser) {
+          done(null, currentUser);
+        } else {
+          const { email, first_name, last_name } = profile._json;
+          await User.create({
+            facebookId: profile.id,
+            email: email,
+            name: `${first_name} ${last_name}`,
+          });
+        }
       }
-    }
-  )
-);
-
+    )
+  );
+}
 export default passport;
