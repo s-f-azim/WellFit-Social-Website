2769cce69c96fb0513745945ff78394b854b8e79
Savraj Bassi <Savrajbassi@gmail.com>
2021-02-27 23:52:49 +0000

    Reduce navbar bottom padding and align mobile menu button

--- frontend/components/Layout.jsx
+++ frontend/components/Layout.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar.jsx";
 const Layout = ({ children }) => {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <main>{children}</main>
     </>
   );
