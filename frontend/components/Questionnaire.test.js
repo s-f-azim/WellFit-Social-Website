7357c61b8d3e6002750e5d2d026dcbea9c340495
Fahim Ahmed <afahim1110@gmail.com>
2021-03-02 16:58:58 +0000

    Comment out mocks

--- frontend/components/Questionnaire.test.js
+++ frontend/components/Questionnaire.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Questionnaire from "./Questionnaire";
 
+/*
 import { useRouter } from "next/router";
 import { updateUser } from "../utils/user.js";
 import { UserContext } from "../contexts/UserContext.js";
@@ -8,8 +9,9 @@ import { UserContext } from "../contexts/UserContext.js";
 jest.mock("next/router");
 jest.mock("../utils/user");
 jest.mock("../contexts/UserContext");
-
+*/
 
 it("works", () => {
+    render(<Questionnaire />)
     expect(2).toBe(2);
 });
