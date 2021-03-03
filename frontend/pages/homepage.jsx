94a6355524e1eb1b0e6dce05971e6d2480c77653
ahmed.alahmadi <a-e-a-1999@hotmail.com>
2021-03-03 16:37:37 +0000

    Add homepage after login

---
+++ frontend/pages/homepage.jsx
@@ -0,0 +1,13 @@
+import {useState} from "react";
+import {Card,Modal,}
+const homePage=()=>{
+    const courses={
+        name:"something",
+        picture:"anoather thing",
+
+    }
+    return (
+    );
+}
+
+
