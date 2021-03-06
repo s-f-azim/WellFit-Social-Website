73eb211a9bb4dc334861f8bbc6fc37cf36d90d03
Alexandre Chouman <alexandre.chouman@outlook.com>
2021-03-06 19:44:06 +0100

    test out axios stuff, obsolete soon

--- frontend/components/AdminDashboard.jsx
+++ frontend/components/AdminDashboard.jsx
@@ -1,7 +1,22 @@
-import { Card } from 'antd';
+import { Card, Row, Statistic } from 'antd';
 import { FundProjectionScreenOutlined } from '@ant-design/icons';
 
+import axios from 'axios';
+import API from '../config';
+
 const AdminDashboard = () => {
+  const getUserCount = () =>
+    axios
+      .get(`${API}/user?role=admin`)
+      .then((response) => {
+        // handle success
+        console.log(response);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+
   const title = (
     <h1>
       <FundProjectionScreenOutlined /> Administrative Dashboard
@@ -10,7 +25,12 @@ const AdminDashboard = () => {
 
   return (
     <div className="adminDashboard">
-      <Card title={title}>hi</Card>
+      {getUserCount()}
+      <Row type="flex" justify="center">
+        <Card title={title}>
+          <Statistic title="Active Users" value={112893} />
+        </Card>
+      </Row>
     </div>
   );
 };
