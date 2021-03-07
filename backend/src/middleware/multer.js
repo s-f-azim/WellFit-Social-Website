0ecd8c9fc23cdb8db24a0af93ce0feda34f8a0e5
ahmed.alahmadi <a-e-a-1999@hotmail.com>
2021-03-07 19:34:10 +0000

    Add multer middleware

---
+++ backend/src/middleware/multer.js
@@ -0,0 +1,20 @@
+import mutler from 'multer';
+import ErrorResponse from '../utils/errorResponse.js';
+
+const upload = multer({
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.orginalname.match(/\.(jpg|jpeg|png)$/))
+      return cb(
+        new ErrorResponse(
+          'Please upload a picture in the format of jpg/jpeg/png',
+          400
+        )
+      );
+    cb(undefined, true);
+  },
+});
+
+export default upload;
