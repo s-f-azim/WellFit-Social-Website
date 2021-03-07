07c63cd43814647d77f2fa6bfd6114e23915a460
jonemilnik <jonathanerivera2501@gmail.com>
2021-03-07 00:07:24 +0000

    Create interactive view for suggestions

--- frontend/pages/profile.jsx
+++ frontend/pages/profile.jsx
@@ -1,7 +1,8 @@
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useAuth } from '../services/auth';
-import { Space, Button, Row } from 'antd';
+import { Space, Button, Row, Card, Col } from 'antd';
+import { CloseOutlined } from '@ant-design/icons';
 
 const Profile = () => {
   const router = useRouter();
@@ -11,29 +12,81 @@ const Profile = () => {
     if (!user) router.push('/');
   }, []);
 
+  const { Meta } = Card;
+
+  
+
+  const Suggestions = () => { //List of suggested instructors
+    //Set up initial list
+    const p1 =  {_id: 1, name: "Jon", followers: 10};
+    const p2 = {_id: 2, name: "Oi", followers:2400};
+    const initialList = [p1, p2];
+    const [list, setList] = useState(initialList);
+    
+    const handleRemove = (id) => { //removes suggested instructor
+      setList(list.filter((item) => item._id !== id));
+    };
+
+    const Suggestion = (props) => { //Suggested instructor element
+      return (
+        <Card 
+            type ="inner"
+            style={{width :300}}
+            actions={[
+                <CloseOutlined key="close" onClick={() => handleRemove(props.user._id)}/> 
+            ]}
+        >
+            <Meta
+                title={props.user.name}
+                description={props.user.followers}
+            />
+        </Card>
+      );
+    };
+    
+    return (
+      <Card title="Suggested Instructors">
+        <Space direction="vertical" size="middle">
+            {list.map((item) =>
+              <Suggestion key={item._id} user={item} />)}
+        </Space>
+      </Card>
+    );
+    
+  }
+
+  
+
   return (
     <div>
-      <Row type="flex" justify="center" align="middle">
-        <Space
-          direction="horizontal"
-          size="large"
-          style={{ border: 'solid', borderWidth: '0.1rem', borderRadius: '1rem' }}
-        >
-          <Button type="link" size="large">
-            Favourites
-          </Button>
-          <Button type="link" size="large">
-            Following
-          </Button>
-          <Button type="link" size="large">
-            Wish List
-          </Button>
-          <Button type="link" size="large">
-            Suggestions
-          </Button>
-        </Space>
+      <Row>
+        <Col push="10">
+        <Row type="flex" align="top" justify="center" align="middle">
+          <Space
+            direction="horizontal"
+            size="large"
+            style={{ border: 'solid', borderWidth: '0.1rem', borderRadius: '1rem' }}
+          >
+            <Button type="link" size="large">
+              Favourites
+            </Button>
+            <Button type="link" size="large">
+              Following
+            </Button>
+            <Button type="link" size="large">
+              Wish List
+            </Button>
+          </Space>
+        </Row>
+        </Col>
+        
+        <Col push="14">
+          <Suggestions />
+        </Col>
       </Row>
+      
     </div>
+    
   );
 };
 
