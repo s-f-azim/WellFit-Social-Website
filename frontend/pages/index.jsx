9cd7e5d395e5f967b39558a7cef759dd6d6383ce
ahmed.alahmadi <a-e-a-1999@hotmail.com>
2021-03-07 17:02:41 +0000

    Fix issue with router

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-nested-ternary */
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import LandingPage from '../components/LandingPage';
 import { useAuth } from '../services/auth';
@@ -6,18 +7,14 @@ import { useAuth } from '../services/auth';
 // eslint-disable-next-line no-unused-vars
 export default function Home() {
   const router = useRouter();
+
   const { user } = useAuth();
+  useEffect(() => {
+    if (user && user.role === 'admin') router.push('/adminDashboard');
+  });
   return (
     <>
-      {user ? (
-        user.role === 'admin' ? (
-          router.push('/adminDashboard', null, { shallow: true })
-        ) : (
-          <LandingPage />
-        )
-      ) : (
-        <LandingPage />
-      )}
+      <LandingPage />
     </>
   );
 }
