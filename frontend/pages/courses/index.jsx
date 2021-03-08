d889ffba4db33ef4dee47e24dc0f47850a4a31f1
ahmed.alahmadi <a-e-a-1999@hotmail.com>
2021-03-08 23:53:52 +0000

    Finish pagination

--- frontend/pages/courses/index.jsx
+++ frontend/pages/courses/index.jsx
@@ -1,17 +1,17 @@
 import { Card, Row, Col, Pagination } from 'antd';
 import Image from 'next/image';
 import Link from 'next/link';
+import Router from 'next/router';
 import api from '../../services/api';
 
 const Courses = ({ courses, total }) => {
-  console.log(courses);
   return (
     <>
       <Row
         type="flex"
         align="middle"
         justify="center"
-        style={{ marginTop: '5rem' }}
+        style={{ margin: '5rem', minHeight: '65vh', padding: '2rem' }}
         gutter={[
           { xs: 8, sm: 16, md: 24, lg: 32 },
           { xs: 8, sm: 16, md: 24, lg: 32 },
@@ -43,8 +43,18 @@ const Courses = ({ courses, total }) => {
           </Link>
         ))}
       </Row>
-      <Row type="flex" justify="center" align="middle" style={{ marginTop: '2rem' }}>
-        <Pagination defaultCurrent={1} total={{ total } / 20 > 0 ? total / 20 : 20} pageSize={20} />
+      <Row
+        type="flex"
+        justify="center"
+        align="middle"
+        style={{ marginTop: '2rem', minHeight: '15vh' }}
+      >
+        <Pagination
+          defaultCurrent={1}
+          total={{ total } / 20 > 0 ? total / 20 : 20}
+          pageSize={2}
+          onChange={(page) => Router.push(`/courses/pages/${page}`)}
+        />
       </Row>
     </>
   );
