cd01a9a898c23d0acba54b8b65b2d41de59f0ce3
Savraj Bassi <Savrajbassi@gmail.com>
2021-03-08 21:04:39 +0000

    Add custom card component

---
+++ frontend/components/CustomCard.jsx
@@ -0,0 +1,52 @@
+import { Card, Row, Col, Modal, Button } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
+import Image from 'next/image';
+import { useState } from 'react';
+
+const CustomCard = () => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <>
+      <Card onClick={() => setVisible(true)} style={{ borderColor: 'black', borderRadius: '1rem' }}>
+        <Row>
+          <Col className="card-title">
+            <h1>Course 1</h1>
+          </Col>
+          <Col>
+            <DeleteOutlined style={{ fontSize: '2rem', color: 'black' }} />
+          </Col>
+        </Row>
+
+        <br />
+
+        <Row style={{ maxHeight: '40rem' }}>
+          <Col className="card-image">
+            <Image src="/jogging.svg" layout="fill" />
+          </Col>
+          <Col>
+            <p>Tags: GetFit</p>
+            <p>Price: £50.00</p>
+            <p>Location: England</p>
+            <p>Creators: One two three</p>
+          </Col>
+        </Row>
+      </Card>
+
+      <Modal
+        title="Course 1"
+        centered
+        visible={visible}
+        onOk={() => setVisible(false)}
+        onCancel={() => setVisible(false)}
+        width={1000}
+      >
+        <p>some contents...</p>
+        <p>some contents...</p>
+        <p>some contents...</p>
+      </Modal>
+    </>
+  );
+};
+
+export default CustomCard;
