2b92af2cf98433d8ee0f89aa488f0ed041778502
Savraj Bassi <Savrajbassi@gmail.com>
2021-03-10 01:34:33 +0000

    Add visual indication of currently selected profile tab

--- frontend/pages/profile.jsx
+++ frontend/pages/profile.jsx
@@ -13,15 +13,83 @@ const Profile = () => {
     if (!user) router.push('/');
   }, []);
 
+  function resetButtons() {
+    ReactDOM.render(
+      <Button
+        id="favourites"
+        type="link"
+        size="large"
+        onClick={displayFavourites}
+        className="button"
+      >
+        Favourites
+      </Button>,
+      document.getElementById('favourites')
+    );
+
+    ReactDOM.render(
+      <Button id="following" type="link" size="large" onClick={displayFollowing} className="button">
+        Following
+      </Button>,
+      document.getElementById('following')
+    );
+
+    ReactDOM.render(
+      <Button id="wishlist" type="link" size="large" onClick={displayWishList} className="button">
+        Wish List
+      </Button>,
+      document.getElementById('wishlist')
+    );
+  }
+
   function displayFavourites() {
+    resetButtons();
+    ReactDOM.render(
+      <Button
+        id="favourites"
+        type="link"
+        size="large"
+        onClick={displayFavourites}
+        className="clicked-button"
+      >
+        Favourites
+      </Button>,
+      document.getElementById('favourites')
+    );
     ReactDOM.render(<p>Favourites</p>, document.getElementById('content'));
   }
 
   function displayFollowing() {
+    resetButtons();
+    ReactDOM.render(
+      <Button
+        id="following"
+        type="link"
+        size="large"
+        onClick={displayFollowing}
+        className="clicked-button"
+      >
+        Following
+      </Button>,
+      document.getElementById('following')
+    );
     ReactDOM.render(<p>Following</p>, document.getElementById('content'));
   }
 
   function displayWishList() {
+    resetButtons();
+    ReactDOM.render(
+      <Button
+        id="wishlist"
+        type="link"
+        size="large"
+        onClick={displayWishList}
+        className="clicked-button"
+      >
+        Wish List
+      </Button>,
+      document.getElementById('wishlist')
+    );
     ReactDOM.render(<WishList user={user} />, document.getElementById('content'));
   }
 
@@ -30,19 +98,37 @@ const Profile = () => {
       <div style={{ width: '80rem' }}>
         <Row type="flex" justify="center" align="middle" className="profile-tabs">
           <Col type="flex" justify="center" align="middle" className="tab">
-            <Button type="link" size="large" onClick={displayFavourites} className="button">
+            <Button
+              id="favourites"
+              type="link"
+              size="large"
+              onClick={displayFavourites}
+              className="button"
+            >
               Favourites
             </Button>
           </Col>
           <Col style={{ width: '2rem' }}></Col>
           <Col type="flex" justify="center" align="middle" className="tab">
-            <Button type="link" size="large" onClick={displayFollowing} className="button">
+            <Button
+              id="following"
+              type="link"
+              size="large"
+              onClick={displayFollowing}
+              className="button"
+            >
               Following
             </Button>
           </Col>
           <Col style={{ width: '2rem' }}></Col>
           <Col type="flex" justify="center" align="middle" className="tab">
-            <Button type="link" size="large" onClick={displayWishList} className="button">
+            <Button
+              id="wishlist"
+              type="link"
+              size="large"
+              onClick={displayWishList}
+              className="button"
+            >
               Wish List
             </Button>
           </Col>
