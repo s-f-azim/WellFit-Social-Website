8a8b906b7c6a13dc95747a63184998a4fb5e811e
Oskar Ribbe <oskar.ribbe@kcl.ac.uk>
2021-03-25 14:04:07 +0100

    add responsive banner 1 for landing page

--- frontend/components/generalComponents/LandingPage/banner-1.jsx
+++ frontend/components/generalComponents/LandingPage/banner-1.jsx
@@ -4,10 +4,13 @@ const Banner1 = () => (
   <div className="banner1">
     <div className="inner_banner1">
       <div className="inner_banner1_content">
-        Connect with health enthusiasts. Enjoy a better lifestyle.
+        <h1 className="headerText">
+          <b>Enjoy a better lifestyle.</b>
+        </h1>
+        <h2>Wellfit helps you connect health enthusiast and learn what works for you.</h2>
       </div>
       <div className="inner_banner1_image">
-        <Image src="/jogging.svg" width="700px" height="700px" />
+        <Image src="/jogging.svg" width="600px" height="600px" />
       </div>
     </div>
   </div>
