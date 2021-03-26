75bb8f6b4777d355bf731c98e0d9d549b33c0925
Fahim Ahmed <afahim1110@gmail.com>
2021-03-26 18:45:26 +0000

    Add ReviewInput tests

--- frontend/__tests__/ReviewInput.test.jsx
+++ frontend/__tests__/ReviewInput.test.jsx
@@ -9,26 +9,31 @@ it('renders fields', () => {
   expect(screen.getByRole('button', { name: 'review' })).toBeInTheDocument();
 });
 
-it.only('calls onSubmit when submitted with valid data', async () => {
+it('calls onSubmit when submitted with valid data', async () => {
   const handleSubmit = jest.fn();
   render(<ReviewInput onSubmit={handleSubmit} />);
 
   const commentField = screen.getByRole('textbox', { name: 'comment' });
   const reviewButton = screen.getByRole('button', { name: 'review' });
 
+  userEvent.click(screen.getAllByRole('radio')[0]);
+
   userEvent.type(commentField, 'test');
   expect(commentField).toHaveValue('test');
 
   userEvent.click(reviewButton);
-  await waitFor(() => expect(handleSubmit).toHaveBeenCalledWith({ rate: '1', comment: 'test' }));
+  await waitFor(() => expect(handleSubmit).toHaveBeenCalledWith({ rate: 1, comment: 'test' }));
+
+  userEvent.click(screen.getAllByRole('radio')[4]);
+  userEvent.click(reviewButton);
+  await waitFor(() => expect(handleSubmit).toHaveBeenCalledWith({ rate: 5, comment: 'test' }));
 });
 
 it('does not call onSubmit when submitted with invalid data', async () => {
   const handleSubmit = jest.fn();
   render(<ReviewInput onSubmit={handleSubmit} />);
 
-  const reviewButton = screen.getByRole('button', { name: 'post' });
-  userEvent.click(reviewButton);
+  userEvent.click(screen.getByRole('button', { name: 'review' }));
 
   await waitFor(() => expect(handleSubmit).toHaveBeenCalledTimes(0));
 });
