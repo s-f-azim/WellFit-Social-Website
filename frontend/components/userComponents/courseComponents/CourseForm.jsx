85e8650cca7ea200eba3f9727e50750d53399a6e
alexcsou <alexandre.chouman@outlook.com>
2021-03-27 20:08:45 +0100

    edit courseform page to look okay and details

--- frontend/components/userComponents/courseComponents/CourseForm.jsx
+++ frontend/components/userComponents/courseComponents/CourseForm.jsx
@@ -10,9 +10,13 @@ import {
   Alert,
   Space,
   Radio,
-  Row,
 } from 'antd';
-import { MinusCircleOutlined, PlusOutlined, CheckOutlined } from '@ant-design/icons';
+import {
+  MinusCircleOutlined,
+  PlusOutlined,
+  CheckOutlined,
+  ProfileOutlined,
+} from '@ant-design/icons';
 
 import { useSession } from 'next-auth/client';
 import createCourse from '../../../actions/course';
@@ -52,205 +56,209 @@ const CourseForm = () => {
     }
   };
 
+  const title = (
+    <h2>
+      Create and Upload a Course <ProfileOutlined />
+    </h2>
+  );
+
   return (
-    <>
-      <Row span={24}>
-        <Card title="Create a course">
-          {error && <Alert type="error" message={error} banner />}
-          <Form
-            name="courseForm"
-            onFinish={onFinish}
-            form={form}
-            layout="vertical"
-            scrollToFirstError
+    <h2>
+      <Card title={title}>
+        {error && <Alert type="error" message={error} banner />}
+        <Form
+          name="courseForm"
+          onFinish={onFinish}
+          form={form}
+          layout="vertical"
+          scrollToFirstError
+        >
+          <Form.Item
+            label="Title:"
+            name="title"
+            rules={[{ required: true, message: 'Please input the title of the course' }]}
           >
-            <Form.Item
-              label="Title:"
-              name="title"
-              rules={[{ required: true, message: 'Please input the title of the course' }]}
-            >
-              <Input />
-            </Form.Item>
+            <Input />
+          </Form.Item>
 
-            <Form.Item
-              label="Description:"
-              name="description"
-              rules={[{ required: true, message: 'Please input the description of the course' }]}
-            >
-              <Input.TextArea rows={3} />
-            </Form.Item>
+          <Form.Item
+            label="Description:"
+            name="description"
+            rules={[{ required: true, message: 'Please input the description of the course' }]}
+          >
+            <Input.TextArea rows={3} />
+          </Form.Item>
 
-            <Form.Item
-              label="Price ($):"
-              name="price"
-              rules={[
-                {
-                  type: 'number',
-                  min: 0,
-                  message: 'Please input a valid price',
-                },
-                { required: true, message: 'Please input the price of the course ($)' },
-              ]}
-            >
-              <InputNumber precision={2} />
-            </Form.Item>
+          <Form.Item
+            label="Price ($):"
+            name="price"
+            rules={[
+              {
+                type: 'number',
+                min: 0,
+                message: 'Please input a valid price',
+              },
+              { required: true, message: 'Please input the price of the course ($)' },
+            ]}
+          >
+            <InputNumber precision={2} />
+          </Form.Item>
 
-            <Form.List name="creators">
-              {(fields, { add, remove }) => (
-                <>
-                  <Form.Item label="Additional Creators:">
-                    {fields.map((field) => (
-                      <>
-                        <Space align="middle">
-                          <Form.Item
-                            {...field}
-                            key={field.key}
-                            validateFirst
-                            rules={[
-                              {
-                                required: true,
-                                whitespace: true,
-                                message: "Please input creator's email or delete this field",
+          <Form.List name="creators">
+            {(fields, { add, remove }) => (
+              <>
+                <Form.Item label="Additional Creators:">
+                  {fields.map((field) => (
+                    <>
+                      <Space align="middle">
+                        <Form.Item
+                          {...field}
+                          key={field.key}
+                          validateFirst
+                          rules={[
+                            {
+                              required: true,
+                              whitespace: true,
+                              message: "Please input creator's email or delete this field",
+                            },
+                            {
+                              type: 'email',
+                              message: 'Please input a valid email',
+                            },
+                            {
+                              validator: async (_, email) => {
+                                if (!email || user.email !== email) return Promise.resolve();
+                                return Promise.reject('You are already a creator');
                               },
-                              {
-                                type: 'email',
-                                message: 'Please input a valid email',
-                              },
-                              {
-                                validator: async (_, email) => {
-                                  if (!email || user.email !== email) return Promise.resolve();
-                                  return Promise.reject('You are already a creator');
-                                },
-                              },
-                            ]}
-                          >
-                            <Input placeholder="example@email.com" />
-                          </Form.Item>
-                          <MinusCircleOutlined onClick={() => remove(field.name)} />
-                        </Space>
-                      </>
-                    ))}
-                    <Form.Item>
-                      <Button type="dashed" onClick={() => add()} icon={<PlusOutlined />}>
-                        Add creator
-                      </Button>
-                    </Form.Item>
+                            },
+                          ]}
+                        >
+                          <Input placeholder="example@email.com" />
+                        </Form.Item>
+                        <MinusCircleOutlined onClick={() => remove(field.name)} />
+                      </Space>
+                    </>
+                  ))}
+                  <Form.Item>
+                    <Button type="dashed" onClick={() => add()} icon={<PlusOutlined />}>
+                      Add creator
+                    </Button>
                   </Form.Item>
-                </>
-              )}
-            </Form.List>
+                </Form.Item>
+              </>
+            )}
+          </Form.List>
 
-            <Form.Item
-              label="Tags:"
-              name="tags"
-              rules={[{ required: true, message: 'Please select at least 1 tag' }]}
-            >
-              <Select mode="multiple" allowClear>
-                {tags.map((tag) => (
-                  <Option key={tag} value={tag}>
-                    {tag}
-                  </Option>
-                ))}
-              </Select>
-            </Form.Item>
+          <Form.Item
+            label="Tags:"
+            name="tags"
+            rules={[{ required: true, message: 'Please select at least 1 tag' }]}
+          >
+            <Select mode="multiple" allowClear>
+              {tags.map((tag) => (
+                <Option key={tag} value={tag}>
+                  {tag}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
 
-            <Form.Item
-              label="Fitness level required:"
-              name="fitnessLevel"
-              rules={[{ required: true, message: 'Please input the required fitness level' }]}
-            >
-              <Select allowClear>
-                <Option value="beginner">Beginner</Option>
-                <Option value="intermediate">Intermediate</Option>
-                <Option value="advanced">Advanced</Option>
-              </Select>
-            </Form.Item>
+          <Form.Item
+            label="Fitness level required:"
+            name="fitnessLevel"
+            rules={[{ required: true, message: 'Please input the required fitness level' }]}
+          >
+            <Select allowClear>
+              <Option value="beginner">Beginner</Option>
+              <Option value="intermediate">Intermediate</Option>
+              <Option value="advanced">Advanced</Option>
+            </Select>
+          </Form.Item>
 
-            <Form.Item
-              label="Training duration (minutes):"
-              name="trainingDuration"
-              rules={[
-                {
-                  type: 'number',
-                  min: 0,
-                  message: 'Please enter positive numbers only',
+          <Form.Item
+            label="Training duration (minutes):"
+            name="trainingDuration"
+            rules={[
+              {
+                type: 'number',
+                min: 0,
+                message: 'Please enter positive numbers only',
+              },
+              {
+                validator: async (_, value) => {
+                  if (!value || Number.isInteger(value)) return Promise.resolve();
+                  return Promise.reject('Please enter a whole number');
                 },
-                {
-                  validator: async (_, value) => {
-                    if (!value || Number.isInteger(value)) return Promise.resolve();
-                    return Promise.reject('Please enter a whole number');
-                  },
-                },
-              ]}
-            >
-              <InputNumber />
-            </Form.Item>
+              },
+            ]}
+          >
+            <InputNumber />
+          </Form.Item>
 
-            <Form.Item
-              name="isVirtual"
-              label="Online course ?"
-              rules={[{ required: true, message: 'Please specify whether this course is online' }]}
-            >
-              <Radio.Group>
-                <Radio value>Yes</Radio>
-                <Radio value={false}>No</Radio>
-              </Radio.Group>
-            </Form.Item>
+          <Form.Item
+            name="isVirtual"
+            label="Online course ?"
+            rules={[{ required: true, message: 'Please specify whether this course is online' }]}
+          >
+            <Radio.Group>
+              <Radio value>Yes</Radio>
+              <Radio value={false}>No</Radio>
+            </Radio.Group>
+          </Form.Item>
 
-            <Form.Item
-              name="gym"
-              label="Requires gym access ?"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please specify whether this course requires a gym access',
-                },
-              ]}
-            >
-              <Radio.Group>
-                <Radio value>Yes</Radio>
-                <Radio value={false}>No</Radio>
-              </Radio.Group>
-            </Form.Item>
+          <Form.Item
+            name="gym"
+            label="Requires gym access ?"
+            rules={[
+              {
+                required: true,
+                message: 'Please specify whether this course requires a gym access',
+              },
+            ]}
+          >
+            <Radio.Group>
+              <Radio value>Yes</Radio>
+              <Radio value={false}>No</Radio>
+            </Radio.Group>
+          </Form.Item>
 
-            <Form.Item
-              label="Training equipment required:"
-              name="trainingEquipment"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please specify at least 1 equipment',
-                },
-              ]}
-            >
-              <Select mode="multiple" allowClear>
-                <Option value="dumbbells">Dumbbells</Option>
-                <Option value="barbells">Barbells</Option>
-                <Option value="resistanceBands">Resistance Bands</Option>
-                <Option value="treadmill">Treadmill</Option>
-                <Option value="cardioMachines">Cardio Machines</Option>
-                <Option value="kettlebells">Kettlebells</Option>
-                <Option value="freeWeights">Free weights</Option>
-                <Option value="battleRopes">Battle Ropes</Option>
-                <Option value="jumRope">Jump rope</Option>
-                <Option value="mats">Training mat</Option>
-                <Option value="abWheel">Ab wheel</Option>
-              </Select>
-            </Form.Item>
+          <Form.Item
+            label="Training equipment required:"
+            name="trainingEquipment"
+            rules={[
+              {
+                required: true,
+                message: 'Please specify at least 1 equipment',
+              },
+            ]}
+          >
+            <Select mode="multiple" allowClear>
+              <Option value="dumbbells">Dumbbells</Option>
+              <Option value="barbells">Barbells</Option>
+              <Option value="resistanceBands">Resistance Bands</Option>
+              <Option value="treadmill">Treadmill</Option>
+              <Option value="cardioMachines">Cardio Machines</Option>
+              <Option value="kettlebells">Kettlebells</Option>
+              <Option value="freeWeights">Free weights</Option>
+              <Option value="battleRopes">Battle Ropes</Option>
+              <Option value="jumRope">Jump rope</Option>
+              <Option value="mats">Training mat</Option>
+              <Option value="abWheel">Ab wheel</Option>
+            </Select>
+          </Form.Item>
 
-            <Form.Item label="Location (if applicable):" name="address">
-              <Input />
-            </Form.Item>
+          <Form.Item label="Location (if applicable):" name="address">
+            <Input />
+          </Form.Item>
 
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
-        </Card>
-      </Row>
-    </>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </h2>
   );
 };
 
--- frontend/pages/postpage.jsx
+++ frontend/pages/postpage.jsx
@@ -1,5 +1,4 @@
 import NotFound from '../components/generalComponents/404';
-
 // legacy page for now...
 export default function PostPage() {
   return <NotFound />;
