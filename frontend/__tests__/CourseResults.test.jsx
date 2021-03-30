7b573be9cda01f5c25f7a7520f7902422beb8211
Fahim Ahmed <afahim1110@gmail.com>
2021-03-30 17:35:59 +0100

    All test cases pass

--- frontend/__tests__/CourseResults.test.jsx
+++ frontend/__tests__/CourseResults.test.jsx
@@ -1,19 +1,40 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import CourseResults from '../components/generalComponents/Search/CourseResults';
+
+import api from '../services/api';
+
+jest.mock('../services/api', () => ({
+  get: jest.fn(),
+}));
 
 const courses = [1, 2, 3, 4].map((n) => ({
   _id: `${n}`,
   title: `course ${n}`,
-  price: n,
+  description: `description ${n}`,
+  price: n * 10,
+  address: `address ${n}`,
   tags: [],
-  trainingEquipment: [],
+  photos: [],
 }));
 
-it.only('renders courses', () => {
-  render(<CourseResults data={courses} />);
+const creators = [1, 2].map((n) => ({
+  _id: `${n}`,
+  fName: `creator ${n}`,
+  lName: `test ${n}`,
+}));
+
+it('renders courses', async () => {
+  api.get.mockReturnValue({
+    data: { success: true, data: creators },
+  });
+
+  await act(async () => {
+    render(<CourseResults data={courses} />);
+  });
 
   courses.forEach((c) => {
     expect(screen.getByText(c.title, { exact: false })).toBeInTheDocument();
     expect(screen.getByText(c.price, { exact: false })).toBeInTheDocument();
+    expect(screen.getByText(c.address, { exact: false })).toBeInTheDocument();
   });
 });
