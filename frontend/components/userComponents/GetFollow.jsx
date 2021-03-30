0fc7d043115c5d1b162b03786fee00a4e81d3dd8
issa-kabir <issa.kabir98@gmail.com>
2021-03-30 22:12:02 +0100

    Remove button from display follow

--- frontend/components/userComponents/GetFollow.jsx
+++ frontend/components/userComponents/GetFollow.jsx
@@ -21,11 +21,10 @@ GetFollow.Item = ({ follow }) => (
     <List.Item key={follow.id}>
       {/* <List.Item.Meta/> */}
       <h5>
-        {`${follow.fName.charAt(0).toUpperCase() + follow.fName.substr(1).toLowerCase()} ${`${
-          follow.lName.charAt(0).toUpperCase() + follow.lName.substr(1).toLowerCase()
-        } `}`}
         <Link href={`/users/${follow._id}`}>
-          <Button>Profile</Button>
+          {`${follow.fName.charAt(0).toUpperCase() + follow.fName.substr(1).toLowerCase()} ${`${
+            follow.lName.charAt(0).toUpperCase() + follow.lName.substr(1).toLowerCase()
+          } `}`}
         </Link>
       </h5>
     </List.Item>
