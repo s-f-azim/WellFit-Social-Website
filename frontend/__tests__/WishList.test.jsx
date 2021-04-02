d690984c2ff72ed704dd168dcc40346a4d3ef5c3
Savraj Bassi <Savrajbassi@gmail.com>
2021-04-02 15:49:49 +0100

    Remove unused import and add additional CourseCard test

--- frontend/__tests__/WishList.test.jsx
+++ frontend/__tests__/WishList.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor, act } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import WishList from '../components/userComponents/WishList';
 import { notification } from 'antd';
