c29ab8f5910861c34fe834d939aa68b4c43fe88e
issa-kabir <issa.kabir98@gmail.com>
2021-04-02 10:38:46 +0100

    Linting done to pass merge

--- frontend/__tests__/InstructorProfile.test.jsx
+++ frontend/__tests__/InstructorProfile.test.jsx
@@ -2,7 +2,6 @@ import { render, screen, act, within, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import UserProfile from '../pages/users/[id]';
 
-
 jest.mock('next-auth/client', () => ({
   useSession: () => [{ user }, false],
 }));
@@ -13,20 +12,50 @@ jest.mock('next/router', () => ({
   }),
 }));
 
-
 afterEach(() => {
   jest.clearAllMocks();
 });
 
-const user = { _id: '1', fName: 'John', lName: 'Wick', following:['2'], qualifications:[], speciality:'', communicationModes:[], paymentFrequency:[], paymentOptions:[], serviceFormat:[], verified:false, gender:'Male', role:'client', trainerType:'', follower:[]};
+const user = {
+  _id: '1',
+  fName: 'John',
+  lName: 'Wick',
+  following: ['2'],
+  qualifications: [],
+  speciality: '',
+  communicationModes: [],
+  paymentFrequency: [],
+  paymentOptions: [],
+  serviceFormat: [],
+  verified: false,
+  gender: 'Male',
+  role: 'client',
+  trainerType: '',
+  follower: [],
+};
 
-const user2 = { _id: '14', fName: 'Alex', lName: 'Mason', following:[], qualifications:[], speciality:'', communicationModes:[], paymentFrequency:[], paymentOptions:[], serviceFormat:[], verified:true, gender:'Male', role:'instructor', trainerType:'', follower:[]};
+const user2 = {
+  _id: '14',
+  fName: 'Alex',
+  lName: 'Mason',
+  following: [],
+  qualifications: [],
+  speciality: '',
+  communicationModes: [],
+  paymentFrequency: [],
+  paymentOptions: [],
+  serviceFormat: [],
+  verified: true,
+  gender: 'Male',
+  role: 'instructor',
+  trainerType: '',
+  follower: [],
+};
 
-
-it("render other users profile", async() => {
-  const { getByText, getByLabelText } = render(<UserProfile user = {user2} />);
-  getByText('Alex Mason', { exact: false }) // Correct display name loaded
-  getByText("Report", {exact: false})	// Report button present on different user profiles
-  getByText("Follow") // Follow button present on different user profiles
-  getByText('Instructor') // correct role
+it('render other users profile', async () => {
+  const { getByText, getByLabelText } = render(<UserProfile user={user2} />);
+  getByText('Alex Mason', { exact: false }); // Correct display name loaded
+  getByText('Report', { exact: false }); // Report button present on different user profiles
+  getByText('Follow'); // Follow button present on different user profiles
+  getByText('Instructor'); // correct role
 });
--- frontend/__tests__/Profile.test.jsx
+++ frontend/__tests__/Profile.test.jsx
@@ -2,7 +2,6 @@ import { render, screen, act, within, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import UserProfile from '../pages/users/[id]';
 
-
 jest.mock('next-auth/client', () => ({
   useSession: () => [{ user }, false],
 }));
@@ -13,25 +12,54 @@ jest.mock('next/router', () => ({
   }),
 }));
 
-
 afterEach(() => {
   jest.clearAllMocks();
 });
 
+const user = {
+  _id: '1',
+  fName: 'John',
+  lName: 'Wick',
+  following: ['2'],
+  qualifications: [],
+  speciality: '',
+  communicationModes: [],
+  paymentFrequency: [],
+  paymentOptions: [],
+  serviceFormat: [],
+  verified: false,
+  gender: 'Male',
+  role: 'client',
+  trainerType: '',
+  follower: [],
+};
 
-const user = { _id: '1', fName: 'John', lName: 'Wick', following:['2'], qualifications:[], speciality:'', communicationModes:[], paymentFrequency:[], paymentOptions:[], serviceFormat:[], verified:false, gender:'Male', role:'client', trainerType:'', follower:[]};
+const user2 = {
+  _id: '14',
+  fName: 'Alex',
+  lName: 'Mason',
+  following: [],
+  qualifications: [],
+  speciality: '',
+  communicationModes: [],
+  paymentFrequency: [],
+  paymentOptions: [],
+  serviceFormat: [],
+  verified: true,
+  gender: 'Male',
+  role: 'instructor',
+  trainerType: '',
+  follower: [],
+};
 
-const user2 = { _id: '14', fName: 'Alex', lName: 'Mason', following:[], qualifications:[], speciality:'', communicationModes:[], paymentFrequency:[], paymentOptions:[], serviceFormat:[], verified:true, gender:'Male', role:'instructor', trainerType:'', follower:[]};
-
-
-it("renders client profile", async() => {
-	await act(async () => {
-    render(<UserProfile user = {user} />);
+it('renders client profile', async () => {
+  await act(async () => {
+    render(<UserProfile user={user} />);
   });
-  screen.getByText('John Wick', { exact: false }) // display name
-  screen.getByText('Unverified User') // verified = false
-  screen.getByText('Client') // role
-  screen.getByText('No bio entered, edit your profile to display it', { exact: false })
-  screen.getByText('Follows 1 other user(s)', { exact: false })
-  screen.getByText('Followed by 0 user(s)', { exact: false })
+  screen.getByText('John Wick', { exact: false }); // display name
+  screen.getByText('Unverified User'); // verified = false
+  screen.getByText('Client'); // role
+  screen.getByText('No bio entered, edit your profile to display it', { exact: false });
+  screen.getByText('Follows 1 other user(s)', { exact: false });
+  screen.getByText('Followed by 0 user(s)', { exact: false });
 });
