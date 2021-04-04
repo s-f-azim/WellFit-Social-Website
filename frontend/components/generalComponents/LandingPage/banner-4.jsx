de502ce38e11c7ed8fa33a884d4f073a48b6c432
Oskar Ribbe <oskar.ribbe@kcl.ac.uk>
2021-04-04 18:11:03 +0200

    add color black to all buttons on front page

--- frontend/components/generalComponents/LandingPage/banner-4.jsx
+++ frontend/components/generalComponents/LandingPage/banner-4.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from 'react';
-import { Card, Anchor } from 'antd';
+import { Card } from 'antd';
 import { useSession } from 'next-auth/client';
 import { getTrendingUsersLimit } from '../../../actions/user';
 
 const { Meta } = Card;
-const { Link } = Anchor;
 
 const Banner4 = () => {
   // List of suggested instructors
