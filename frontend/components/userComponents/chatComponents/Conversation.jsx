42314c3a5c53ac568b810761088134ecd21c6846
ahmed-alahmadi <a-e-a-1999@hotmail.com>
2021-04-05 19:10:24 +0100

    Fix chat bug

--- frontend/components/userComponents/chatComponents/Conversation.jsx
+++ frontend/components/userComponents/chatComponents/Conversation.jsx
@@ -42,7 +42,7 @@ const Conversation = ({ conversation }) => {
   // check for new messages and update the state
   useEffect(() => {
     socket.on('message', (message) => {
-      setMsgs([...msgs, message]);
+      if (conversation.users.includes(message.author)) setMsgs([...msgs, message]);
     });
     scrollToBottom();
   }, [msgs]);
