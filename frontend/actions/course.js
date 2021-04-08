bb8b4743410fb831edceeb0fc43fa251ca78196d
Fahim Ahmed <afahim1110@gmail.com>
2021-04-08 14:18:57 +0100

    Implement course photo uploading

--- frontend/actions/course.js
+++ frontend/actions/course.js
@@ -1,9 +1,15 @@
+/* eslint-disable no-underscore-dangle */
 import api from '../services/api';
 
 const createCourse = async (values) => {
   const res = await api.post('/courses/create', {
     ...values,
   });
+  const course = res.data.data;
+  const formData = new FormData();
+  formData.append('images', values.photo.originFileObj);
+  await api.post(`/courses/${course._id}/images`, formData);
+
   return res.data;
 };
 
@@ -24,5 +30,6 @@ const getCourses = (title, tags, etags, pageSize, offset) =>
 
 const getCourseCreators = (courseId) => api.get(`/courses/${courseId}/creators`);
 
+const uploadImages = (id, images) => api.post(`/courses/${id}/images`, images);
 
 export { createCourse as default, deleteCourse, getCourses, getCourseCreators };
